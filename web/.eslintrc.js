88c67a9e3e33d859be777149f119ea1e98b21996
Luis Oliveira <luiseduardobo2011@gmail.com>
2020-12-24 22:22:33 -0300

    correcting linting

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,45 +1,25 @@
 module.exports = {
-  env : {
-    es2021 : true,
-    node : true,
-    browser : true,
-  },
-  extends : [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser : '@typescript-eslint/parser',
-  parserOptions : {
-    ecmaFeatures : {
-      jsx : true,
-      tsx : true,
+    parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+    extends: [
+        'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+        'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+        'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+        'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    ],
+    parserOptions: {
+        ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
+        ecmaFeatures: {
+            jsx: true, // Allows for the parsing of JSX
+        },
     },
-    ecmaVersion : 12,
-    sourceType : 'module',
-  },
-  plugins : [
-    'react',
-    '@typescript-eslint',
-  ],
-  rules : {
-    'react/jsx-filename-extension' : [1, { extensions : ['.js', '.jsx', '.tsx'] }],
-    'react/no-unescaped-entities' : 0,
-    '@typescript-eslint/no-use-before-define' : 'off',
-    'no-use-before-define' : 'off',
-    'linebreak-style' : 0,
-    'no-irregular-whitespace' : 'off',
-    'import/extensions' : 'off',
-    quotes : ['error', 'single'],
-    'key-spacing' : ['error', { beforeColon : true, afterColon : true }],
-    'react/jsx-one-expression-per-line' : 'off',
-    'import/prefer-default-export' : 'off',
-    'no-unused-vars' : 'off',
-  },
-  settings : {
-    'import/resolver' : {
-      node : {
-        extensions : ['.js', '.jsx', '.ts', '.tsx'],
-      },
+    rules: {
+        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
-  },
+    settings: {
+        react: {
+            version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+        },
+    },
 };
